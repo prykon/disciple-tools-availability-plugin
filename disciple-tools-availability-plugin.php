7ddee81810951d409f387b595e4cc9fce1b48568
Prykon <dariomanoukian@gmail.com>
2023-05-08 10:04:49 -0300

    Fixed 'tile input not being saved' bug

--- disciple-tools-availability-plugin.php
+++ disciple-tools-availability-plugin.php
@@ -3,7 +3,7 @@
  *Plugin Name: Disciple.Tools - Availability
  * Plugin URI: https://github.com/prykon/disciple-tools-availability-plugin
  * Description: Easily find the best time slot for a group with many members.
- * Version:  1.1.3
+ * Version:  1.1.4
  * Author URI: https://github.com/prykon
  * GitHub Plugin URI: https://github.com/prykon/dt-availability-plugin
  * Requires at least: 4.7.0
@@ -178,7 +178,7 @@ class DT_Contact_Availability {
                                         <?php
                                         $class = ( in_array( $option_key, $post[$field_key] ?? [] ) ) ?
                                             "selected-select-button" : "empty-select-button"; ?>
-                                      <button id="<?php echo esc_html( $option_key ) ?>" type="button" data-field-key="<?php echo esc_html( $field_key ); ?>"
+                                      <button id="<?php echo esc_html( $option_key ) ?>" value="<?php echo esc_html( $option_key ) ?>" type="button" data-field-key="<?php echo esc_html( $field_key ); ?>"
                                               class="dt_multi_select <?php echo esc_html( $class ) ?> select-button button " style="padding:5px">
                                           <?php echo esc_html( $post_fields[$field_key]["default"][$option_key]["label"] ) ?>
                                       </button>
